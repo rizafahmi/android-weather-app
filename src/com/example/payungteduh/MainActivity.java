25c43640ef169cfad08914f35e413f6f87ec50c8
Riza Fahmi <rizafahmi@gmail.com>
2016-05-18 22:46:11 +0700

    Request using synchronous method

--- src/com/example/payungteduh/MainActivity.java
+++ src/com/example/payungteduh/MainActivity.java
@@ -1,15 +1,40 @@
 package com.example.payungteduh;
 
+import java.io.IOException;
+
 import android.app.Activity;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends Activity {
+	
+	private final String TAG = MainActivity.class.getSimpleName();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		String forecastUrl = "http://api.openweathermap.org/data/2.5/weather?q=Ciledug&APPID=b15b9835567a918fbec1d1c6e67f347a";
+		
+		OkHttpClient client = new OkHttpClient();
+		Request request = new Request.Builder().url(forecastUrl).build();
+		Call call = client.newCall(request);
+		
+		try {
+			Response response = call.execute();
+			if (response.isSuccessful()) {
+				Log.v(TAG, response.body().string());
+			}
+		} catch (IOException e) {
+			Log.e(TAG, "Exception caught: ", e);
+		}
 	}
 }
